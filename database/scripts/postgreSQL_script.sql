d9919cdf17e5d5096b5a7d534d139fe83a7d27cc
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-24 16:03:33 -0600

    Fix SQL table script

--- database/scripts/postgreSQL_script.sql
+++ database/scripts/postgreSQL_script.sql
@@ -1,35 +1,110 @@
-CREATE TABLE events (
-    event_id SERIAL PRIMARY KEY,
-    time DECIMAL NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    person VARCHAR(255),
-	link VARCHAR(255),
-	actType VARCHAR(255),
-	legMode VARCHAR(255),
-    vehicle VARCHAR(255),
-    networkMode VARCHAR(255),
-    distance DECIMAL,
-	mode VARCHAR(255),
-    x_coordinate DECIMAL,
-    y_coordinate DECIMAL,
-    amount DECIMAL,
-    purpose VARCHAR(255),
-	transactionPartner VARCHAR(255),
+CREATE TABLE Node (
+	"node id" INT PRIMARY KEY,
+	x DECIMAL,
+	y DECIMAL
+);
+
+CREATE TABLE Link (
+	"link id" VARCHAR(255) PRIMARY KEY,
+	from INT,
+	to INT,
+	freespeed DECIMAL,
+	capacity DECIMAL,
+	permlanes DECIMAL,
+	oneway BOOLEAN,
+	modes TEXT,
+	FOREIGN KEY (from) REFERENCES Node("node id"),
+	FOREIGN KEY (to) REFERENCES Node("node id")
+);
+
+CREATE TABLE "event" (
+	eventID SERIAL PRIMARY KEY,
+	time DECIMAL NOT NULL,
+	person VARCHAR(255),
+	vehicle VARCHAR(255)
+);
+CREATE TABLE actEnd (
+	eventID INT,
+	FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+	type VARCHAR(255)
+);
+CREATE TABLE dvrpTask (
+	eventID INT,
+	FOREIGN KEY (eventID) REFERENCES "event"(eventID),
 	dvrpVehicle VARCHAR(255),
 	taskType VARCHAR(255),
-	taskIndex INT,
-    dvrpMode VARCHAR(255),
-	relativePosition DECIMAL,
-	atStop Decimal,
-	destinationStop VARCHAR(255),
-    agent VARCHAR(255),
-    facility VARCHAR(255),
-    delay INT,
-	vehicleID VARCHAR(255),
-	transitRouteId VARCHAR(255),
-	departureId VARCHAR(255),
-	transitLineId VARCHAR(255),
-	driverId VARCHAR(255),
-	request VARCHAR(255)
+	taskIndex VARCHAR(255),
+	dvrpMode VARCHAR
 );
-end;
+CREATE TABLE EnterLeftLink (
+	eventID INT,
+	FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+	start BOOLEAN
+);
+CREATE TABLE actStart (
+	eventID INT,
+	FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+	type VARCHAR(255),
+	x DECIMAL,
+	y DECIMAL
+);
+CREATE TABLE arrivalDeparture (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+legmode VARCHAR(255),
+start BOOLEAN
+);
+CREATE TABLE vehicleTraffic (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+relativePosition DECIMAL,
+networkMode VARCHAR(255),
+start BOOLEAN
+);
+CREATE TABLE travelled (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+mode VARCHAR(255),
+distance DECIMAL
+);
+CREATE TABLE transitDriverStarts (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+driverId VARCHAR(255),
+vehicleId VARCHAR(255),
+departureId VARCHAR(255),
+transitLineId VARCHAR(255),
+transitRouteId VARCHAR(255)
+);
+CREATE TABLE passengerPickDrop (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+request VARCHAR(255),
+mode VARCHAR(255),
+start BOOLEAN
+);
+CREATE TABLE vehicleFacility (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+delay DECIMAL,
+facility VARCHAR(255)
+);
+CREATE TABLE personVehicle (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+start BOOLEAN
+);
+CREATE TABLE waitingForPt (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+destinationStop DECIMAL,
+atStop DECIMAL,
+agent VARCHAR(255)
+);
+CREATE TABLE personMoney (
+eventID INT,
+FOREIGN KEY (eventID) REFERENCES "event"(eventID),
+transactionPartner VARCHAR(255),
+amount DECIMAL,
+purpose VARCHAR(255)
+);
