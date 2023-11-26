84747bb75060fc3a2026d0b661ce3b0ff15fc0f8
oppo266 <8pp8413@gmail.com>
2023-11-26 05:16:15 -0600

    Update Question4Queries.sql

--- database/queries/Question4Queries.sql
+++ database/queries/Question4Queries.sql
@@ -44,19 +44,3 @@ SELECT person, min(event_time)
 FROM event_data NATURAL JOIN arrival_departure
 WHERE legmode = 'car' AND is_start = FALSE
 GROUP BY person;
-
--- 4B
-SELECT
-    link.*,
-    from_node.x as from_node_x,
-    from_node.y as from_node_y,
-    to_node.x as to_node_x,
-    to_node.y as to_node_y
-FROM
-    link
-JOIN
-    node AS from_node ON link.from_node = from_node.node_id
-JOIN
-    node AS to_node ON link.to_node = to_node.node_id
-WHERE
-    link.link_id = '7735018_0';
